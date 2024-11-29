1c94e9ca5dfc7022be7b37f0c16d1998fcadfc8b
Mu Ye <emailyemu@gmail.com>
2024-11-28 23:54:23 -0800

    3.1

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+
 	// 1. Request URLs are automatically sanitized, e.g /foo/bar/..//baz redirects to /foo/baz
 	// 2. If /foo/ is registered, then /foo redirects to /foo/
 	mux := http.NewServeMux()
@@ -18,9 +22,9 @@ func main() {
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
-	log.Print("starting server on :4000")
+	log.Printf("starting server on %s", *addr)
 
 	// This runs an infinite loop and only returns when an error occurs
-	err := http.ListenAndServe(":4000", mux)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }
