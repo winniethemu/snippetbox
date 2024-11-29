ad467a3d1d5d1d047109bd7cfb10bc8206fc38c4
Mu Ye <emailyemu@gmail.com>
2024-11-28 17:35:09 -0800

    2.3

--- main.go
+++ main.go
@@ -9,9 +9,21 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a form for creating a new snippet..."))
+}
+
 func main() {
+	// 1. Request URLs are automatically sanitized, e.g /foo/bar/..//baz redirects to /foo/baz
+	// 2. If /foo/ is registered, then /foo redirects to /foo/
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
+	mux.HandleFunc("/{$}", home) // Restricting subtree path matching
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Print("starting server on :4000")
 
