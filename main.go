c04e37f8e0d922706d78fece0febfd9a3918543e
Mu Ye <emailyemu@gmail.com>
2024-11-28 16:14:22 -0800

    2.2

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from Snippetbox"))
+}
 
 func main() {
-	fmt.Println("Hello world!")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	log.Print("starting server on :4000")
+
+	// This runs an infinite loop and only returns when an error occurs
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 }
