1bfeff6d1cce1d1b8ad2c8622d6f366cc569e64f
Mu Ye <emailyemu@gmail.com>
2024-11-29 12:37:05 -0800

    3.3

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,19 +7,27 @@ import (
 	"os"
 )
 
+type application struct {
+	logger *slog.Logger
+}
+
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
+	app := &application{
+		logger: logger,
+	}
+
 	// 1. Request URLs are automatically sanitized, e.g /foo/bar/..//baz redirects to /foo/baz
 	// 2. If /foo/ is registered, then /foo redirects to /foo/
 	mux := http.NewServeMux()
-	mux.HandleFunc("GET /{$}", home) // Restricting subtree path matching
-	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
-	mux.HandleFunc("GET /snippet/create", snippetCreate)
-	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
+	mux.HandleFunc("GET /{$}", app.home) // Restricting subtree path matching
+	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
+	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
+	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
 
 	// Static assets
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
