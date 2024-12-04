5efcf1c0de780c1c55685168e0a7f8eacaac8595
Mu Ye <emailyemu@gmail.com>
2024-12-04 10:43:14 -0800

    4.4

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log/slog"
 	"net/http"
 	"os"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
 type application struct {
@@ -17,6 +20,14 @@ func main() {
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
+	dsn := "host=localhost port=5432 user=snippetbox database=snippetbox sslmode=disable"
+	db, err := openDB(dsn)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+	defer db.Close()
+
 	app := &application{
 		logger: logger,
 	}
@@ -24,7 +35,23 @@ func main() {
 	logger.Info("starting server", "addr", *addr)
 
 	// This runs an infinite loop and only returns when an error occurs
-	err := http.ListenAndServe(*addr, app.routes())
+	err = http.ListenAndServe(*addr, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
+}
+
+// Wraps sql.Open() and returns a sql.DB connection pool for a given Data Source Name
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
 }
