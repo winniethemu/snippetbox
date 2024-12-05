91c51985b6393e41079d59b2407de182fe4df931
Mu Ye <emailyemu@gmail.com>
2024-12-04 16:18:17 -0800

    4.6

---
+++ internal/models/snippets.go
@@ -0,0 +1,40 @@
+package models
+
+import (
+	"database/sql"
+	"time"
+)
+
+type Snippet struct {
+	ID      int
+	Title   string
+	Content string
+	Created time.Time
+	Expires time.Time
+}
+
+type SnippetModel struct {
+	DB *sql.DB
+}
+
+func (m *SnippetModel) Insert(title string, content string, expires int) (int, error) {
+	stmt := `INSERT INTO snippets (title, content, created, expires)
+	VALUES ($1, $2, NOW() AT TIME ZONE 'UTC', NOW() AT TIME ZONE 'UTC' + $3 * INTERVAL '1 day')
+	RETURNING id`
+
+	var id int
+	err := m.DB.QueryRow(stmt, title, content, expires).Scan(&id)
+	if err != nil {
+		return 0, err
+	}
+
+	return id, nil
+}
+
+func (m *SnippetModel) Get(id int) (Snippet, error) {
+	return Snippet{}, nil
+}
+
+func (m *SnippetModel) Latest() ([]Snippet, error) {
+	return nil, nil
+}
